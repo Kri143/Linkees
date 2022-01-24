af14668bd9f069e85c2c7b261073ff13343a697e
Kristian <78504847+Kri143@users.noreply.github.com>
2022-01-24 19:17:51 +0100

    Add files via upload

---
+++ src/assets/items.js
@@ -0,0 +1,75 @@
+import website from '../assets/website.png'
+import instagram from '../assets/instagram.png'
+import appstore from '../assets/playstore.png'
+import youtube from '../assets/youtube.jpeg'
+import dribbble from '../assets/dribbble.png'
+import github from '../assets/github.png'
+import telegram from '../assets/telegram.png'
+import twitter from '../assets/twitter.png'
+import whatsapp from '../assets/whatsapp.png'
+import hype from '../assets/hype.png'
+
+const items = [{
+    "title": "Il mio sito",
+    "subtitle": "Guarda che figo!",
+    "image": website,
+    "link": "https://kristianburiascophotos.tk" //your personal website or portfolio  link
+},
+{
+    "title": "Il mio blog",
+    "subtitle": "Vieni a commentare!",
+    "image": website,
+    "link": "https://blog.kristianburiascophotos.gq" //your personal website or portfolio  link
+},
+{
+    "title": "GitHub",
+    "subtitle": "@kri143 anche se non pubblico molto...",
+    "image": github,
+    "link": "https://github.com/kri143" //Github Profile link
+},
+{
+    "title": "Instagram",
+    "subtitle": "@_kri14_ | Profilo personale",
+    "image": instagram,
+    "link": "https://instagram.com/_kri14_" //instagram profile link 
+},
+{
+    "title": "Instagram",
+    "subtitle": "@kristianburiascophotos | Profilo Delle foto",
+    "image": instagram,
+    "link": "https://instagram.com/kristianburiascophotos" //instagram profile link 
+},
+{
+    "title": "Twitter",
+    "subtitle": "@kri141 | Anche se non lo uso è figo averne uno",
+    "image": twitter,
+    "link": "https://twitter.com/kri141"// twitter profile link 
+},
+{
+    "title": "YouTube",
+    "subtitle": "@kri14 | Non pubblico da mesi ma se vuoi iscriviti",
+    "image": youtube,
+    "link": "https://www.youtube.com/channel/UCMLhkyKyLWaLG-yq1c1jzPQ"//youtube channel link 
+},
+{
+    "title": "Telegram",
+    "subtitle": "@kri14 | Se hai bisogno subito usa questo o whatsapp ",
+    "image": telegram,
+    "link": "https://telegram.me/kri14" //Telegram Pofile 
+}
+{
+    "title": "Whatsapp",
+    "subtitle": "Profilo business",
+    "image": whatsapp,
+    "link": "https://wa.me/message/DBULR6MKK7J2O1" //whatsapp
+},
+{
+    "title": "Hype",
+    "subtitle": "Ottima carta prepagata Te la consiglio",
+    "image": hype,
+    "link": "https://www.hype.it/landing-mgm?proof=75416253514e45714250773d&proofType=hype&promoCode=INV-75416253514e45714250773d&utm_source=mgm&utm_medium=75416253514e45714250773d" //hype link
+
+},
+]
+
+export default items
